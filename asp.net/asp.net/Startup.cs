a624a4541c20eca52aa91897408a8279f5576fef
Yonglun Liu <lyl3366@gmail.com>
2015-08-27 10:09:17 -0500

    first work website

---
+++ asp.net/asp.net/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp.net.Startup))]
+namespace asp.net
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
